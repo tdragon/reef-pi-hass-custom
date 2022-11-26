3a0b79c7b029a1ef604326287a946cfb8c822bd6
Dmitrii <1210261+tdragon@users.noreply.github.com>
2022-11-26 12:06:43 +0200

    Update release script

--- scripts/prepare_release.sh
+++ scripts/prepare_release.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
-cd "$(dirname "${BASH_SOURCE[0]}")"/../custom_components
+cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
-if [ -f ../output/reef_pi.zip ]; then
-    rm ../output/reef_pi.zip
+if [ -f ./output/reef_pi.zip ]; then
+    rm ./output/reef_pi.zip
 fi
-mkdir -p ../output
-zip -x "*.pyc" -x "reef_pi/__pycache__/" -r ../output/reef_pi.zip reef_pi
+mkdir -p ./output
+cd custom_components/reef_pi/
+zip -x "*.pyc" -x "reef_pi/__pycache__/" -r ../../output/reef_pi.zip ./
