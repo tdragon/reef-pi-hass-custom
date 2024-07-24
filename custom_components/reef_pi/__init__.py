c95da1abb61fa317647e34b125cae517518c0810
tdragon <1210261+tdragon@users.noreply.github.com>
2024-07-24 18:51:19 +0300

    fix: Deprecation warning
    Closes #28

--- custom_components/reef_pi/__init__.py
+++ custom_components/reef_pi/__init__.py
@@ -62,10 +62,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         "undo_update_listener": undo_listener,
     }
 
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
---
+++ tests/test_logs.py
@@ -0,0 +1,43 @@
+"""Test Ph sensor for Reef_Pi integration."""
+
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+
+
+from custom_components.reef_pi import DOMAIN
+
+
+import pytest
+import respx
+from . import async_api_mock
+
+
+@pytest.fixture
+async def async_api_mock_instance():
+    with respx.mock() as mock:
+        async_api_mock.mock_all(mock)
+        yield mock
+
+
+async def test_no_warning_in_log(hass, async_api_mock_instance, caplog):
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "host": async_api_mock.REEF_MOCK_URL,
+            "username": async_api_mock.REEF_MOCK_USER,
+            "password": async_api_mock.REEF_MOCK_PASSWORD,
+            "verify": False,
+        },
+    )
+
+    entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    lines = caplog.text.split("\n")
+    warnings = [
+        line
+        for line in lines
+        if ("WARNING" in line or "ERROR" in line)
+        and "We found a custom integration reef_pi" not in line
+    ]
+    assert len(warnings) == 0
